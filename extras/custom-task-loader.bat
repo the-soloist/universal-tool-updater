88c902cbcc0996ad4252617230a7c451e6b62304
Sp1k3 <46804083+the-soloist@users.noreply.github.com>
2026-08-28 02:03:27 +0800

    chore: remove legacy task scripts

--- extras/custom-task-loader.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd /D "%~dp0"
-
-set "UPDATER=%~dp0updater.exe"
-if not exist "%UPDATER%" set "UPDATER=%~dp0..\updater.exe"
-
-echo Executing Universal Tool Updater
-"%UPDATER%" --profiles "%~dp0..\profiles" update > last-run.log 2>&1
